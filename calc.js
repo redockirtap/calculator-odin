205bbc95bbc04075f5c284797b0f2b3d6ff3a13c
Partrickoder <redockirtap@gmail.com>
2022-08-27 12:14:33 +0300

    Create two functions add(...args) and multiply(...args).

--- calc.js
+++ calc.js
@@ -1,1 +1,20 @@
 console.log('I am alive!');
+
+// Basic math logic goes here
+const add = function(...args) {
+    let total = 0;
+    for (let x of args) {
+        total += x;
+    }
+    return total;
+}
+
+const multiply = function(...args) {
+    if(args.length === 0) return 0;
+    let total = 1;
+    for (let x of args) {
+        total *= x;
+    }
+    return total;
+}
+
