d84577b401caecf0658a1df6fadec9b616318910
Partrickoder <redockirtap@gmail.com>
2022-08-30 20:19:21 +0300

    Add more math operations - subtract and divide. Change the parameters of all the functions from (...args) to (num1, num2). Add function operate(operator, num1, num2), which calls the required function to work with two numbers.

--- calc.js
+++ calc.js
@@ -1,19 +1,34 @@
-console.log('I am alive!');
-
 // Basic math logic goes here
-const add = function(...args) {
-    let total = 0;
-    for (let x of args) {
-        total += x;
-    }
+const add = function(num1, num2) {
+    let total = num1 + num2;
     return total;
 }
 
-const multiply = function(...args) {
-    if(args.length === 0) return 0;
-    let total = 1;
-    for (let x of args) {
-        total *= x;
+const multiply = function(num1, num2) {
+    let total = num1 * num2;
+    return total;
+}
+
+const subtract = function(num1, num2) {
+    let total = num1 - num2;
+    return total;
+}
+const divide = function(num1, num2) {
+    let total = num1 / num2;
+    if (total === Infinity) return 'Naaah, leave it.';
+    return total;
+}
+
+const operate = function(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '/':
+            return divide(num1, num2);
     }
     return total;
 }
